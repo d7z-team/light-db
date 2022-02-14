ffcbf57cc7735878f750016febe72feb02fc2c59
dragon <33776693+ExplodingDragon@users.noreply.github.com>
2022-02-14 19:55:10 +0800

    feat(db-mod/session): 完成 session 模块

--- api-modules/mod-session/src/test/kotlin/org/d7z/light/db/modules/session/LightSessionTest.kt
+++ api-modules/mod-session/src/test/kotlin/org/d7z/light/db/modules/session/LightSessionTest.kt
@@ -1,6 +1,7 @@
 package org.d7z.light.db.modules.session
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 
 internal class LightSessionTest {
@@ -11,9 +12,10 @@ internal class LightSessionTest {
         val newSession = build.findSessionGroupContext().newSession()
         println(newSession.sessionId)
         println(newSession.updateTime)
-        val findSessionGroupContext = build.findSessionGroupContext()
-        val querySession = findSessionGroupContext.querySession(newSession.sessionId)
+        val find = build.findSessionGroupContext()
+        val querySession = find.querySession(newSession.sessionId)
         assertEquals(querySession.get().createTime, newSession.createTime)
-        findSessionGroupContext.destroy(newSession.sessionId)
+        find.destroy(newSession.sessionId)
+        assertFalse(find.querySession(newSession.sessionId).isPresent)
     }
 }
